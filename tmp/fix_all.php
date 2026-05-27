e8622f3134adb87dd64afe7dbc5542a982af009d
rassameldeeb4 <rassameldeeb4@gmail.com>
2026-05-27 06:43:44 +0300

    add showlog to fix_all file list

--- tmp/fix_all.php
+++ tmp/fix_all.php
@@ -53,6 +53,7 @@ $files = [
     'resources/views/admin/exercises/create.blade.php',
     'resources/views/admin/exercises/edit.blade.php',
     'tmp/fix_storage.php',
+    'tmp/showlog.php',
 ];
 $ok = 0; $fail = 0;
 foreach ($files as $f) {
