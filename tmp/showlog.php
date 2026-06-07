5e0242b9b55bc1d9456c77042b3d0e4a34a01572
rassameldeeb4 <rassameldeeb4@gmail.com>
2026-06-07 06:06:45 +0300

    fix: expand showlog.php to show full log with ?all=1

--- tmp/showlog.php
+++ tmp/showlog.php
@@ -2,7 +2,16 @@
 $log = __DIR__ . '/../storage/logs/laravel.log';
 if (!file_exists($log)) { echo "No log file\n"; exit; }
 $lines = file($log);
-$show = array_slice($lines, -60);
+$showAll = ($_GET['all'] ?? '') === '1';
+if ($showAll) {
+    $show = $lines;
+} else {
+    $take = max(200, count($lines));
+    $show = array_slice($lines, -$take);
+}
 echo "<pre>";
 foreach ($show as $l) echo htmlspecialchars($l);
 echo "</pre>";
+if ($showAll) {
+    echo "<p>END</p>";
+}
